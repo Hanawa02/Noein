82ee1240501e6fc6b842f3e422ca9a09f07ad84f
Hanawa02 <laura.c.r.melo@gmail.com>
2016-07-15 17:28:21 -0300

    Criação do Projeto
    O projeto Noein foi criado

---
+++ Noein/Noein/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Noein.Startup))]
+namespace Noein
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
